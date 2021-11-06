d1b5d229703e9f836d6e1901bc7c2c2a562be726
aditya37 <aditrahman212@gmail.com>
2021-11-07 02:38:48 +0700

    Add response

--- app/Http/Controllers/Content/AddContent.php
+++ app/Http/Controllers/Content/AddContent.php
@@ -16,22 +16,37 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Exception;
 
+// TODO: rubah return ke func consistenReturn
 class AddContent extends Controller {
+    function consistenReturn($msg,$code,$data) {
+        if ($data == "") {
+            return response()->json([
+                'message' => $msg, 
+                'code' => $code,
+                'data' => array() 
+            ],$code);    
+        }
+        return response()->json([
+            'message' => $msg, 
+            'code' => $code,
+            'data' => $data
+         ],$code);
+    }
     public function index(Request $request) {
         
         $validator = Validator::make($request->all(),[
             'product_id' => 'required',
             'title' => 'required',
             'description' => 'required',
-            'modified_by' => 'required',
+            'created_by' => 'required',
         ],[
             'product_id.required' => 'Please fill product_id',
             'title.required' => 'Please fill title',
             'description.required' => 'Please fill description',
-            'modified_by.required' => 'Please fill modified by'
+            'created_by.required' => 'Please fill modified by'
         ]);
 
-        if (!$validator->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'message' => $validator->errors(),
                 'code' => 500,
@@ -41,7 +56,7 @@ class AddContent extends Controller {
         
         try {
             // validation user id
-            $isValidUser = MstUser::find($request->modified_by);    
+            $isValidUser = MstUser::find($request->created_by);    
             if (!$isValidUser) {
                 return response()->json([
                     'message' => "User id not registered, please use valid user id",
@@ -63,13 +78,22 @@ class AddContent extends Controller {
             // insert content
             $content = MstContent::create([
                 'product_id' => $request->product_id,
-                'modified_by' => $request->modified_by,
+                'created_by' => $request->created_by,
                 'title' => $request->title,
                 'description' => $request->description
             ]);
+            
             // TODO: Add response
-            var_dump($content);
+            $arrData = [];
+            array_push($arrData,array(
+                'id' => $content->id,
+                'created_by' => $request->created_by,
+                'created_at' => time(),
+            ));
+
+            return $this->consistenReturn("Success create new post",201,$arrData);
         } catch (Exception $e) {
+            var_dump($e);
             return response()->json([
                 'message' => 'Failed add content',
                 'code' => 500,
